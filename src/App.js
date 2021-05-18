5d6afdf2219b6b984b2adbf544bd3c77ced9a104
tkozemzak <timkozemzak@gmail.com>
2021-05-18 11:47:40 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useFriction } from "renature";
 
 function App() {
+  const [props] = useFriction({
+    from: {
+      transform: "translateX(100px)",
+    },
+    to: {
+      transform: "translateX(300px)",
+    },
+    config: {
+      mu: 0.8,
+      mass: 2,
+      initialVelocity: 5,
+    },
+    repeat: Infinity,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div {...props} className="box" />
     </div>
   );
 }
